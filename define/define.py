e82842379147e9ac153c1c8d8a1f09d3d688102b
Mouleshwar <mouli.4543@gmail.com>
2015-02-20 17:24:11 +0530

    handle 'no word given' and 'unknown word given' errors

--- define/define.py
+++ define/define.py
@@ -2,16 +2,20 @@ import sys
 from api import apiUrl,apiKey
 from wordnik import swagger,WordApi
 
-word = sys.argv[1];
+if len(sys.argv)<=1:
+    print "No word given to search for. Usage 'define <word>'."
+else:
+    word = sys.argv[1];
+    #Initialise client
+    client = swagger.ApiClient(apiKey, apiUrl);
 
-#Initialise client
-client = swagger.ApiClient(apiKey, apiUrl);
-
-wordApi = WordApi.WordApi(client);
-definitions = wordApi.getDefinitions(word,sourceDictionaries='wiktionary');
-if not definitions:
-	print "Trying for "+word.lower();
-	definitions = wordApi.getDefinitions(word.lower(),sourceDictionaries='wiktionary');
-
-for definition in definitions:
-	print definition.partOfSpeech+": "+definition.text;
+    wordApi = WordApi.WordApi(client);
+    definitions = wordApi.getDefinitions(word,sourceDictionaries='wiktionary');
+    if not definitions:
+    	    print "Trying for "+word.lower();
+	    definitions = wordApi.getDefinitions(word.lower(),sourceDictionaries='wiktionary');
+    if definitions==None:
+        print "No such word found. Check the spelling and try again"
+    else:
+        for definition in definitions:
+	    print definition.partOfSpeech+": "+definition.text;
